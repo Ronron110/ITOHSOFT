4241e766b5f25ed05bd0be378ca72cbda069dab2
ITOMA <>
2020-05-04 18:19:22 +0900

    ファンを仕掛けた

---
+++ PJ_TimeAndDimension/Assets/fanBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fanBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion rot = Quaternion.AngleAxis(20 * Time.timeScale, Vector3.forward);
+        // 現在の自信の回転の情報を取得する。
+        Quaternion q = this.transform.rotation;
+        // 合成して、自身に設定
+        this.transform.rotation = q * rot;
+    }
+}
