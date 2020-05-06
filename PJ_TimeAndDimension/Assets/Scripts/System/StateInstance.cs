d8d7d20e4213d5489d6fcb19b03fde468879ffde
Shigedaruma <shige2011gt@gmail.com>
2020-05-06 17:09:41 +0900

    add: 必要な基礎ソースたちをアップ

---
+++ PJ_TimeAndDimension/Assets/Scripts/System/StateInstance.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ステートインスタンス（抽象クラス）
+/// </summary>
+/// <remarks>
+/// 状態遷移システムの中の一つのインスタンス
+/// Enter - 状態がこのインスタンスに遷移したときに最初に呼ばれる想定
+/// Update - このインスタンスが実行中に毎フレーム呼ばれる想定
+/// Leave - 状態が別のインスタンスに変わるとき、この状態としては最後に呼ばれる想定
+/// 
+/// category と name はこのような相似形のクラスを使う場合
+/// インスタンスの特定が困難になることが多いので、
+/// インスタンスごとに名前を付けられるようにしてある。
+/// </remarks>
+public abstract class StateInstance
+{
+    /// <summary>カテゴリ名</summary>
+    protected string category;
+
+    /// <summary>インスタンス名</summary>
+    protected string name;
+
+    /// <summary>導入処理</summary>
+    public abstract void Enter();
+
+    /// <summary>アップデート処理</summary>
+    public abstract void Update();
+
+    /// <summary>終了処理</summary>
+    public abstract void Leave();
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="c">カテゴリ名</param>
+    /// <param name="n">名前</param>
+    public StateInstance(string c, string n)
+    {
+        this.category = c;
+        this.name = n;
+    }
+}
---
+++ PJ_TimeAndDimension/Assets/Scripts/System/Transition.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ゲーム状態を遷移するオブジェクト
+/// </summary>
+public class Transition : StateInstance
+{
+    #region StateInstances
+    /// <summary>ゲーム状態</summary>
+    /// <TODO>partial 化する</TODO>
+    private Dictionary<string, StateInstance> gameStates = new Dictionary<string, StateInstance>()
+    {
+    };
+
+    /// <summary>スタートアップステート</summary>
+    private const string kStartupState = "Splash";
+    #endregion
+
+    /// <summary>次のステート</summary>
+    private KeyValuePair<string, StateInstance>? nextState = null;
+    /// <summary>現在のステート</summary>
+    private KeyValuePair<string, StateInstance>? currentState = null;
+
+    /// <summary>
+    /// ステート遷移進入処理
+    /// </summary>
+    public override void Enter()
+    {
+        this.RequestChangeState(kStartupState);
+    }
+
+    /// <summary>
+    /// ステート内部処理
+    /// </summary>
+    public override void Update()
+    {
+        // ステート処理実行
+        this.currentState?.Value.Update();
+
+        // 次のステートへの変更があったら
+        if(this.nextState != null)
+        {
+            // 現在のステートに退出処理を実行
+            this.currentState?.Value.Leave();
+            // 次のステートに変更するのでステート進入処理を実行
+            this.nextState.Value.Value.Enter(); // 手前の Value は nullable の Value
+            // 現在のステートを切り替え
+            this.currentState = this.nextState;
+            // 変更要求はクリアしておく
+            this.nextState = null;
+        }
+    }
+
+    /// <summary>
+    /// 終了処理
+    /// </summary>
+    public override void Leave()
+    {
+
+    }
+
+    /// <summary>
+    /// ステートの変更要求
+    /// </summary>
+    /// <param name="state">ステート名</param>
+    /// <remarks>
+    /// 変更要求がダブった場合後から要求したものが採用される
+    /// </remarks>
+    public void RequestChangeState(string state)
+    {
+        if(this.nextState != null)
+        {
+            Debug.LogWarning("The reserved next state " + this.nextState.Value.Key + "is discarded");
+        }
+        this.nextState = new KeyValuePair<string, StateInstance>(state, gameStates[state]);
+    }
+
+    public Transition() : base("System", "Transition")
+    {
+
+    }
+}
+
